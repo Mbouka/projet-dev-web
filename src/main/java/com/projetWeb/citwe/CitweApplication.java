12a74dbfa8fa0d45aca1ec8fee74a3fecb110b9d
socpa linda <linda.socpa@institutsaintjean.org>
2022-05-19 13:13:02 +0100

    projet spring citwe

---
+++ src/main/java/com/projetWeb/citwe/CitweApplication.java
@@ -0,0 +1,13 @@
+package com.projetWeb.citwe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitweApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitweApplication.class, args);
+	}
+
+}
