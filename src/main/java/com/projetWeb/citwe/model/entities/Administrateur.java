9a98fb28adb8208b7c55e334d831a23602c31362
Tagne237 <100275512+Tagne237@users.noreply.github.com>
2022-05-27 18:56:01 +0000

    j'ai ajouté des attributs dans la classe chambre , je cherhce à savoir si on ne cree pas plutot une seule classe user au lieu de diviser en plusieurs

--- src/main/java/com/projetWeb/citwe/model/entities/Administrateur.java
+++ src/main/java/com/projetWeb/citwe/model/entities/Administrateur.java
@@ -1,4 +1,14 @@
 package com.projetWeb.citwe.model.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Admin")
 public class Administrateur {
+
 }
--- src/main/java/com/projetWeb/citwe/model/entities/Chambre.java
+++ src/main/java/com/projetWeb/citwe/model/entities/Chambre.java
@@ -1,4 +1,24 @@
 package com.projetWeb.citwe.model.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Chambre {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private Long Numchambre;
+    private Long Numcompteureau;
+    private Long Numcompteurele;
+    private String statut;
 }
