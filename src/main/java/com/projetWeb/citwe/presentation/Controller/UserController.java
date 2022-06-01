18d5b5e811043559016e0e4b47027e0370fc4fe0
Tagne237 <100275512+Tagne237@users.noreply.github.com>
2022-06-01 11:30:56 +0000

    Creation des Fragments pour le user et pour l'admin avec modification, ajout des th block sur toutes les pages

---
+++ src/main/java/com/projetWeb/citwe/presentation/Controller/UserController.java
@@ -0,0 +1,23 @@
+package com.projetWeb.citwe.presentation.Controller;
+
+import com.projetWeb.citwe.service.Ichambre;
+import com.projetWeb.citwe.service.Ietudiant;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+public class UserController {
+    @Autowired
+     Ichambre ichambre;
+    @Autowired
+    Ietudiant ietudiant;
+    @GetMapping("/")
+    public String pageAccueil(Model model) {
+        return "index";
+    }
+
+}
--- src/main/java/com/projetWeb/citwe/presentation/api/ChambreRestController.java
+++ src/main/java/com/projetWeb/citwe/presentation/api/ChambreRestController.java
@@ -5,6 +5,7 @@ import com.projetWeb.citwe.service.Ichambre;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
